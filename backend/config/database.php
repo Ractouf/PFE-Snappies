6ce5ef4b788f4c7d66a474ca2e55ec0bf09b5c29
francois.vandeputte <francois.vandeputte@student.vinci.be>
2023-12-07 13:15:50 +0100

    feat: changed default pgsql settings

--- backend/config/database.php
+++ backend/config/database.php
@@ -66,10 +66,10 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'coursinfo.vinci.be'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'dbfrancois_vandeputte'),
+            'username' => env('DB_USERNAME', 'francois_vandeputte'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
