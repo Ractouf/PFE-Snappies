5884f5599c3c7e7634cbb587ecff4110fb9714e3
Damien <lapinskidamien@gmail.com>
2023-12-13 23:27:03 +0100

    fix: is_admin navbar links

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -10,27 +10,30 @@ const Header = () => {
     return (
         <header>
 
-            <div className="logo"><img src="/src/assets/logo.png" alt="Logo" className="logo-img"/></div>
+            <div class="logo"><img src="/src/assets/logo.png" alt="Logo" class="logo-img"/></div>
             <p>Page Name</p>
 
-            <input className="side-menu" type="checkbox" id="side-menu"/>
-            <label className="hamb" htmlFor="side-menu"><span className="hamb-line"></span></label>
+            <input class="side-menu" type="checkbox" id="side-menu"/>
+            <label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
 
-            <nav className="nav">
-                <ul className="menu">
-                    {isAdmin && (
-                        <li className="subnav">
-                            <p className="subnavbtn">Admin <i className="fa fa-caret-down"></i></p>
-                            <div className="subnav-content">
-                                <a href="#">Tournées</a>
-                                <a href="#">Clients</a>
-                                <a href="#">Articles</a>
-                                <a href="#">Livreurs</a>
-                            </div>
-                        </li>
-                    )}
-                    <li><a href="#">Inventaire</a></li>
-                    <li><a href="#">Changer Tournée</a></li>
+            <nav class="nav">
+                <ul class="menu">
+                    {isAdmin ? (
+                            <li class="subnav">
+                                <p class="subnavbtn">Admin <i class="fa fa-caret-down"></i></p>
+                                <div class="subnav-content">
+                                    <a href="#">Tournées</a>
+                                    <a href="#">Clients</a>
+                                    <a href="#">Articles</a>
+                                    <a href="#">Livreurs</a>
+                                </div>
+                            </li>
+                        )
+                        : <>
+                            <li><a href="#">Inventaire</a></li>
+                            <li><a href="#">Changer Tournée</a></li>
+                        </>
+                    }
                     <li><a href="#">Déconnexion</a></li>
                 </ul>
             </nav>
